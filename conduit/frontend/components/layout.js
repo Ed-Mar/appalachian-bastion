18b8124d13ccb4ea79c049c61291e7b1d4f23637
Ed-Mar <kaiservonmar@protonmail.com>
2021-12-11 16:54:32 -0500

    Adding basic frontend Nextjs

---
+++ conduit/frontend/components/layout.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="Conduit"
+          content=""
+        />
+      </Head>
+      <header className={styles.header}>
+        <Link href="/">
+          <h1>conduit</h1>
+        </Link>
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  )
+}
