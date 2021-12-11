f146e28bb59c6391c1b1fd600fd75c436bc24b2d
Ed-Mar <kaiservonmar@protonmail.com>
2021-12-11 16:55:10 -0500

    warning fixing

--- conduit/frontend/components/layout.js
+++ conduit/frontend/components/layout.js
@@ -1,18 +1,18 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+
+        <link rel="icon" href="/public/favicon.ico" />
         <meta
           name="Conduit"
           content=""
         />
+          <title>Conduit</title>
       </Head>
       <header className={styles.header}>
         <Link href="/">
