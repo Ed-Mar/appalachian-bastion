d3704302d2f994e71eb70ecd23f8cfda4c39282b
Ed__ <edward.marable@gmail.com>
2022-07-07 06:58:39 -0400

    Updates to Channel PUT handler to just make it exist cause it didn't

--- conduit/backend/channel-service/handlers/put.go
+++ conduit/backend/channel-service/handlers/put.go
@@ -7,33 +7,46 @@ import (
 )
 
 // swagger:route PUT /servers/{id}/channels channels updateChannel
-// Update a channels details
+// UpdateWithoutIDParm a channels details
 //
 // responses:
 //	201: noContentResponse
 //  404: errorResponse
 //  422: errorValidation
 
-// Update handles PUT requests to update channels
-func (channel *Channels) Update(rw http.ResponseWriter, r *http.Request) {
+// UpdateWithoutIDParm handles PUT requests to update channels
+func (channel *Channels) UpdateWithoutParms(rw http.ResponseWriter, r *http.Request) {
 	rw.Header().Add("Content-Type", "application/json")
 
 	// fetch the channel from the context
-	serv := r.Context().Value(KeyChannel{}).(*models.Channel)
-	channel.APILogger.Println("[DEBUG] updating record id", serv.ID)
-
-	err := models.UpdateChannel(*serv)
-	if err == models.ErrChannelNotFound {
-		channel.APILogger.Println("[ERROR] channel not found", err)
+	leChannel := r.Context().Value(KeyChannel{}).(*models.Channel)
+	channel.APILogger.Println("[DEBUG] updating record id", leChannel.ID)
+	// Sending the Channel info to try and be updated
+	err := models.UpdateChannel(*leChannel)
+	switch err {
+	// It is updated has happened and all gone as epected
+	case nil:
+		rw.WriteHeader(http.StatusOK)
+		return
+	// The updated was tried, but was not found either to never existing or has been soft-deleted
+	case models.ErrChannelNotFound:
 
 		rw.WriteHeader(http.StatusNotFound)
-		err := internal.ToJSON(&GenericError{Message: "channel not found in database"}, rw)
+		err := internal.ToJSON(GenericError{Message: err.Error()}, rw)
 		if err != nil {
-			channel.APILogger.Println(err)
+			channel.APILogger.Println("[ERROR] in JSON encoding: ", err)
+			return
+		}
+		return
+	//some fucky-wucky has occurred, granted I did not leave a lot of options to be caught
+	default:
+		channel.APILogger.Println("[ERROR Unexpected Error when trying to update this channel: " + leChannel.ID.String())
+		rw.WriteHeader(http.StatusInternalServerError)
+		err := internal.ToJSON(GenericError{Message: err.Error()}, rw)
+		if err != nil {
+			channel.APILogger.Println("[ERROR] in JSON encoding: ", err)
+			return
 		}
 		return
 	}
-
-	// write the no content success header
-	rw.WriteHeader(http.StatusNoContent)
 }
