9f0e9a209cc395cd46afb86372224f5dd8e27fe1
Ed__ <edward.marable@gmail.com>
2022-07-08 05:39:58 -0400

    Updated the Delete handler to use the internal GetPram instead of internal one made for Server at dev start

--- conduit/backend/server-service/handlers/delete.go
+++ conduit/backend/server-service/handlers/delete.go
@@ -2,55 +2,65 @@ package handlers
 
 import (
 	"backend/internal"
+	"backend/internal/helper"
 	"backend/server-service/models"
 	"net/http"
 )
 
 // swagger:route DELETE /servers/{id} servers deleteServer
-// Update a servers details
+// UpdateSingleton a servers details
 //
 // responses:
 //	201: noContentResponse
 //  404: errorResponse
 //  501: errorResponse
 
-// Delete handles DELETE requests and removes items from the database
-func (server *Servers) Delete(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Add("Content-Type", "application/json")
-	id, err := getServerID(r)
-	if err != nil {
+// DeleteSingleton handles DELETE requests and removes items from the database
+func (server *Servers) DeleteSingleton(rw http.ResponseWriter, r *http.Request) {
+	//Checks frist if that id is even the in database before getting to try and delete it
+	leServerID, err := helper.GetUUIDFromReqParm(r, "serverID")
+	switch err {
+	case nil: //Not Error
+	case helper.ErrIncorrectUUIDFormat: // Format Error with UUID passed
 		rw.WriteHeader(http.StatusBadRequest)
 		err := internal.ToJSON(&GenericError{Message: err.Error()}, rw)
 		if err != nil {
-			server.severAPILogger.Println("[ERROR] JSON: ", err)
+			// if encode to JSON fails just logged from the JSON side
 			return
+		}
+		return
+	default: // Catch all error
+		rw.WriteHeader(http.StatusInternalServerError)
+		err := internal.ToJSON(&GenericError{Message: err.Error()}, rw)
+		if err != nil {
+			server.APILogger.Println("[ERROR] in JSON encoding: ", err)
 		}
 		return
 	}
 
-	server.severAPILogger.Println("[DEBUG] deleting record id", id)
-
-	err = models.DeleteServer(id)
+	server.APILogger.Println("[DEBUG] deleting record id", leServerID)
+	//Sends the server Id to be deleted to the model function, should erturn
+	err = models.DeleteServer(leServerID)
 	switch err {
 	case nil:
+		rw.WriteHeader(http.StatusNoContent)
 	case models.ErrServerNotFound:
-		server.severAPILogger.Println("[ERROR] deleting record id does not exist")
+		server.APILogger.Println("[ERROR] deleting record id does not exist")
 		rw.WriteHeader(http.StatusNotFound)
 		err := internal.ToJSON(&GenericError{Message: err.Error()}, rw)
 		if err != nil {
-			server.severAPILogger.Println("[ERROR] in JSON encoding: ", err)
+			server.APILogger.Println("[ERROR] in JSON encoding: ", err)
 			return
 		}
 		return
 	default:
-		server.severAPILogger.Println("[ERROR] deleting record", err)
+		server.APILogger.Println("[ERROR] deleting record", err)
 		rw.WriteHeader(http.StatusInternalServerError)
 		err := internal.ToJSON(&GenericError{Message: err.Error()}, rw)
 		if err != nil {
-			server.severAPILogger.Println("[ERROR] in JSON encoding: ", err)
+			server.APILogger.Println("[ERROR] in JSON encoding: ", err)
 			return
 		}
 		return
 	}
-	rw.WriteHeader(http.StatusNoContent)
 }
