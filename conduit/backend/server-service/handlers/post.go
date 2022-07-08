e4f51d4dabd91d99f75754d14366db5a4dc8d906
Ed__ <edward.marable@gmail.com>
2022-07-08 05:44:54 -0400

    Added overwite of the incoming status of the server to be pending in prep for kafka integration

--- conduit/backend/server-service/handlers/post.go
+++ conduit/backend/server-service/handlers/post.go
@@ -6,26 +6,26 @@ import (
 )
 
 // swagger:route POST /servers servers createServer
-// Create a new servers
+// CreateSingleton a new servers
 //
 // responses:
 //	200: ServerResponse
 //  422: errorValidation
 //  501: errorResponse
 
-// Create handles POST requests to add new servers
-func (server *Servers) Create(rw http.ResponseWriter, r *http.Request) {
+// CreateSingleton handles POST requests to add new servers
+func (server *Servers) CreateSingleton(rw http.ResponseWriter, r *http.Request) {
 	// fetch the servers from the context
 	rw.Header().Add("Content-Type", "application/json")
 
-	srev := r.Context().Value(KeyServer{}).(*models.Server)
-	server.severAPILogger.Println("[DEBUG] Inserting Server")
-	//TODO remove this later
-	if len(srev.Status) < 0 {
-		srev.Status = "FAKE STATUS"
-	}
-	/////
-	err := models.AddServer(*srev)
+	//Gets the passed JSON and coverts it's into a Server Struct
+	leserver := r.Context().Value(KeyServer{}).(*models.Server)
+	server.APILogger.Println("[DEBUG] Inserting Server")
+
+	//Overwrites the income server.Status cause all item need to be put in pending until no issues can be confirmed then it will be updated to READY
+	leserver.Status = "PENDING"
+
+	err := models.AddServer(*leserver)
 	if err != nil {
 		return
 	}
