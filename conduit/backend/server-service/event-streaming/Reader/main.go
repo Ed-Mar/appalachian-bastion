21d4d694d1a7a99607409047ecdfe686e9178db4
Ed__ <edward.marable@gmail.com>
2022-07-11 21:49:57 -0400

    a basic reader with the ability to close correctly

---
+++ conduit/backend/server-service/event-streaming/Reader/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	consumerGroups "backend/event-streaming/kafka/consumer-groups"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	reader := consumerGroups.ServiceEventBus
+	for {
+		m, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			break
+		}
+		fmt.Printf("message at topic/partition/offset %v/%v/%v: %s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
+	}
+
+	// trap sigterm or interrupt and gracefully shutdown the servers
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	err := reader.Close()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
