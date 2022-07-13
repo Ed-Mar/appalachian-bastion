f3c5487d09d56386396fd05a7ba41b2b1d77014b
Ed__ <edward.marable@gmail.com>
2022-07-13 19:19:41 -0400

    Gonna thrwo the to and from topics in this so I only have to pass the message

--- conduit/backend/event-streaming/kafka/messages/model/event-message.go
+++ conduit/backend/event-streaming/kafka/messages/model/event-message.go
@@ -9,20 +9,20 @@ import (
 type EventMessage struct {
 	// Planning on assigning each of the Saga Orchestrators an ID, not sure how I am going to keep track of each
 	// of them.
-	SagaOrchestratorID int `json:"SagaOrchestrationID"`
-	// Not positive this really necessary besides readability in the logs if something fucks up
 	SagaOrchestratorName string `json:"SagaOrchestratorName"`
 	// In the case that of a transaction triggering other event-messages to other services this will stay to allow for the
 	// propagates back to it to finish this transaction. Not that positive if this will be needed, but maybe
 	SagaTransactionOriginID uuid.UUID `json:"SagaTransactionOriginID"`
+
+	FromServiceTopic   string `json:"FromServiceTopic"`
+	TargetServiceTopic string `json:"TargetServiceTopic"`
+
 	// This is the ID of the Transaction sending the request
-	SagaTransactionID uuid.UUID `json:"SagaTransactionID"`
+	SagaTransactionActionID uuid.UUID `json:"SagaTransactionID"`
 	// This is the ID of the Transaction sending the request
 	SagaTransactionName string `json:"SagaTransactionName"`
 	// Which Service is this message intended for
 	ServiceTargetName string `json:"ServiceTargetName"`
-	// Which Service ID is this message intended for
-	ServiceTargetID int `json:"ServiceTargetID"`
 	// Which Service Operation is message intended for
 	ServiceTargetOperation string `json:"ServiceTargetOperation"`
 	//Putting the time of the creation of this message in the obj so that  I get better understanding the delay
